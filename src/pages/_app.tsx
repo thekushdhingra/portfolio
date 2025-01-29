930afbffbfac1f899ad145abd6a701c9bdfdfc9b
Kush <kushdhingra3112@gmail.com>
2025-01-29 17:21:49 +0530

    main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "prismjs/themes/prism.css";
-import "@/styles/code.css"
+import "@/styles/code.css";
+import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Kush Dhingra</title>
+        <meta name="description" content="Kush Dhingra" />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"/>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,9 +3,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Kush Dhingra</title>
-        <meta name="description" content="Kush Dhingra" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"/>
       </Head>
       <body className="min-h-[100vh] bg-custom-bg text-white bg-block-pattern bg-[size:block-pattern] bg-[position:block-pattern]">
         <Main />
