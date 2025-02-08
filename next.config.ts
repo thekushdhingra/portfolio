e535f1bf826752ec2082c1c5071a4c928bd8f566
thekushdhingra <kushdhingra3112@gmail.com>
2025-02-08 13:34:31 +0530

    main

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
+        hostname: "github.com"
+      },
+      {
+        protocol: "https",
         hostname: "http.cat",
       },
     ],
